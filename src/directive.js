df490a20ceb18cf371745b325dcca29b01b7a091
Jaime Bunzli <jpbunzli@gmail.com>
2016-01-22 14:22:54 -0300

    feat(refactor): the directive creates the wrapper

--- src/directive.js
+++ src/directive.js
@@ -1,116 +1,136 @@
 (function(){
   'use strict';
 
+  var INIT_CLASS = 'pretty-load-init';
+  var LOADING_CLASS = 'pretty-load-loading';
+  var COMPLETED_CLASS = 'pretty-load-completed';
+  
   angular
     .module('platanus.prettyLoad')
     .directive('prettyLoad', prettyLoad);
 
   /* ngInject */
   function prettyLoad() {
-    var ALWAYS_CLASS = 'pretty-loader';
-    var LOADING_CLASS = 'pretty-loading';
-    var LOADED_CLASS = 'pretty-loaded';
-
-    var checkLoaded = function(_image) {
-      if (!_image.complete) return false;
-      if (_image.naturalWidth === 0) return false;
-      return true;
-    };
-
-    var monitorLoadState = function(_parent, _image) {
-      var checkInterval = setInterval(function(){
-        if ( checkLoaded(_image) ) {
-          _parent.removeClass(LOADING_CLASS);
-          _parent.addClass(LOADED_CLASS);
-          clearInterval(checkInterval);
-        }
-      }, 200);
-      _parent.addClass(LOADING_CLASS);
-    };
-
-    var setupOverlay = function(_parent) {
-      var overlay = angular.element('<div class="pretty-loader-overlay"></div>');
-      _parent.append(overlay);
-
-      overlay.css({ 
-        position: 'absolute',
-        backgroundColor: 'white'
-      });
-
-      return overlay;
-    }
-
-    var resizeImage = function(_image, _originalWidth, _originalHeight) {
-      var ratio = _originalWidth / _originalHeight;
-
-      if ( _image.width === 0 && _image.height === 0 ) {
-        _image.width = _originalWidth;
-        _image.height = _originalHeight;
-      } else if ( _image.clientWidth > 0 ) {
-        _image.height = _image.clientWidth / ratio;
-      } else {
-        _image.width = _image.clientHeight * ratio;
-      }
-    };
-
-    var positionOverlay = function(_parent, _target, _overlay) {
-      if(!_overlay) return;
-
-      var parentRect = _parent.getBoundingClientRect(),
-          targetRect = _target.getBoundingClientRect(),
-          offsetTop = targetRect.top - parentRect.top,
-          offsetLeft = targetRect.left - parentRect.left;
-
-      _overlay.css({
-        top: offsetTop + 'px',
-        left: offsetLeft + 'px',
-        width: _target.clientWidth + 'px',
-        height: _target.clientHeight + 'px'
-      });
-    }
 
     return {
       restrict: 'A',
+
       scope: {
         prettyLoadWidth: '@',
         prettyLoadHeight: '@'
       },
+
       link: function(_scope, _element, _attributes) {
-        var image = _element.find('img')[0];
-        if ( !image ) return false;
-        
-        _element.addClass(ALWAYS_CLASS);
+        var settings = {
+          referenceWidth: _scope.prettyLoadWidth,
+          referenceHeight: _scope.prettyLoadHeight,
+          referenceColor: _attributes.prettyLoadColor || "white",
+          shouldSetImageWidth: _element[0].width == 0,
+          shouldSetImageHeight: _element[0].height == 0
+        };
 
-        monitorLoadState(_element, image);
-        var overlay = setupOverlay(_element);
+        var image = _element;
+        var wrapper = createWrapper(image);
+        var overlay = createOverlay(wrapper, settings.referenceColor);
 
-        _attributes.$observe('prettyLoadColor', function(color){
-          overlay.css({ 
-            backgroundColor: color
-          });
-        });
+        setImageSize(image, settings);
+        setOverlayPositionAndSize(overlay, image);
 
-        var recalculateDimensions = function(){
-          resizeImage(image, _scope.prettyLoadWidth, _scope.prettyLoadHeight);
-          positionOverlay(_element[0], image, overlay);
-        }; 
-
-        _scope.$watch(function() { 
-          if ( _scope.prettyLoadWidth && _scope.prettyLoadHeight ) {
-            return _scope.prettyLoadWidth / _scope.prettyLoadHeight; 
-          } else return 0;
-        }, function(_ratio) {
-          if ( _ratio == 0 ) return;
-          recalculateDimensions();
-        });
-        
-        window.addEventListener('resize', recalculateDimensions);
-
-        _scope.$on('$destroy', function() {
-          window.removeEventListener('resize', recalculateDimensions)
-        });
+        monitorLoading(wrapper, image);
+        watchResizeEvent(_scope, image, overlay, settings);
       }
     }
   }
+
+  //// Private Methods
+
+  function createWrapper(_image) {
+    var html = "<div class='pretty-load-wrapper'></div>";
+    _image.wrap(html);
+
+    var wrapper = _image.parent();
+
+    wrapper.css({
+      position: "relative"
+    });
+
+    return wrapper;
+  }
+
+  function createOverlay(_wrapper, _referenceColor) {
+    var html = '<div class="pretty-load-overlay"></div>';
+    var overlay = angular.element(html);
+
+    _wrapper.append(overlay);
+
+    overlay.css({
+      position: 'absolute',
+      backgroundColor: _referenceColor
+    });
+
+    return overlay;
+  }
+
+  function monitorLoading(wrapper, image) {
+    setTimeout(function() {
+      wrapper.addClass(LOADING_CLASS);
+
+      whenImageIsLoaded(image, function() {
+        wrapper.removeClass(LOADING_CLASS);
+        wrapper.addClass(COMPLETED_CLASS);
+      });
+    }, 0);
+  }
+
+  function watchResizeEvent(_scope, _image, _overlay, _settings) {
+    var callback = function() {
+      setImageSize(_image, _settings);
+      setOverlayPositionAndSize(_overlay, _image);
+    }
+
+    window.addEventListener('resize', callback);
+
+    _scope.$on('$destroy', function() {
+      window.removeEventListener('resize', callback)
+    });
+  }
+
+  function setImageSize(_image, _settings) {
+    var imageNode = _image[0];
+    if(_settings.shouldSetImageWidth && _settings.shouldSetImageHeight) {
+      imageNode.width = _settings.referenceWidth;
+      imageNode.height = _settings.referenceHeight;
+    }
+    else {
+      var ratio = _settings.referenceWidth / _settings.referenceHeight;
+
+      if(_settings.shouldSetImageWidth)   imageNode.width = imageNode.clientHeight * ratio;
+      if(_settings.shouldSetImageHeight)  imageNode.height = imageNode.clientWidth / ratio;
+    }
+  };
+
+  function setOverlayPositionAndSize(_overlay, _image) {
+    _overlay.css({
+      top: '0px',
+      left: '0px',
+      width: _image[0].clientWidth + 'px',
+      height: _image[0].clientHeight + 'px'
+    });
+  }
+
+  function checkLoaded(_image) {
+    if (!_image.complete) return false;
+    if (_image.naturalWidth === 0) return false;
+    return true;
+  };
+
+  function whenImageIsLoaded(_image, _callback) {
+    var checkInterval = setInterval(function(){
+      if ( checkLoaded(_image[0]) ) {
+        clearInterval(checkInterval);
+        _callback.call();
+      }
+    }, 200);
+  };
 
 })();
